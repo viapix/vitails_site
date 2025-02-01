813433fa446209b93c7645c07c9ffbe64e68c854
viapix <via@vitails.dev>
2025-02-01 06:38:42 +0000

    styling changes

--- src/components/MainPageComponent.vue
+++ src/components/MainPageComponent.vue
@@ -4,10 +4,8 @@ import { RouterView } from 'vue-router'
 
 <template>
   <main class="px-[15%] bg-linear-to-t from-zinc-800 to-gray-700">
-    <div>
-      <div class="border-2">
-        <RouterView />
-      </div>
+    <div class="bg-zinc-900 h-full">
+      <RouterView />
     </div>
   </main>
 </template>
